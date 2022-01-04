6156c9d34acac6721b46ff5e7510b7157c449c71
抹茶爱吃猹 <gh@cha.moe>
2022-01-04 22:34:15 +0800

    Update single.py

--- single.py
+++ single.py
@@ -88,61 +88,61 @@ fake_slic_path = slic_path + 'fake/'
 real_slic_path = slic_path + 'real/'
 
 # %%
-fake_start_count = math.ceil(0.8 * len(fake_image_filenames))  # fake起始
-real_start_count = math.ceil(0.8 * len(real_image_filenames))  # real起始
-print('正在运行图像处理：fake')
-start_count = 0
-train_processeed_list = get_image_file_list(path_exist('../SLIC_result/60/validation/fake/'), is_train_data=True)
-if len(train_processeed_list) != 0:
-        start_count = int(train_processeed_list[-1].split('/')[-1].split('_')[0])
-for i in tqdm(range(start_count, len(fake_image_filenames[fake_start_count:]))):
-    img = io.imread(fake_image_filenames[fake_start_count:][i])
-    segments = slic(img, 40, 10)
-    color_dictionary = {}
-    for row in range(segments.shape[0]):
-        for col in range(segments.shape[1]):
-            if segments[row][col] not in color_dictionary:
-                color_dictionary[segments[row][col]] = [(row, col)]
-            else:
-                color_dictionary[segments[row][col]].append((row, col))
+# fake_start_count = math.ceil(0.8 * len(fake_image_filenames))  # fake起始
+# real_start_count = math.ceil(0.8 * len(real_image_filenames))  # real起始
+# print('正在运行图像处理：fake')
+# start_count = 0
+# train_processeed_list = get_image_file_list(path_exist('../SLIC_result/60/validation/fake/'), is_train_data=True)
+# if len(train_processeed_list) != 0:
+#         start_count = int(train_processeed_list[-1].split('/')[-1].split('_')[0])
+# for i in tqdm(range(start_count, len(fake_image_filenames[fake_start_count:]))):
+#     img = io.imread(fake_image_filenames[fake_start_count:][i])
+#     segments = slic(img, 40, 10)
+#     color_dictionary = {}
+#     for row in range(segments.shape[0]):
+#         for col in range(segments.shape[1]):
+#             if segments[row][col] not in color_dictionary:
+#                 color_dictionary[segments[row][col]] = [(row, col)]
+#             else:
+#                 color_dictionary[segments[row][col]].append((row, col))
 
-    for color in color_dictionary.keys():
-        path = path_exist(slic_path + 'fake/') + "{}_{}.jpg".format(i, color)
-        data_copy = img
-        for row in range(data_copy.shape[0]):
-            for col in range(data_copy.shape[1]):
-                if (row, col) in color_dictionary[color]:
-                    continue
-                else:
-                    data_copy[row][col] = (0, 0, 0)
-        io.imsave(path, data_copy, check_contrast=False)
+#     for color in color_dictionary.keys():
+#         path = path_exist(slic_path + 'fake/') + "{}_{}.jpg".format(i, color)
+#         data_copy = img
+#         for row in range(data_copy.shape[0]):
+#             for col in range(data_copy.shape[1]):
+#                 if (row, col) in color_dictionary[color]:
+#                     continue
+#                 else:
+#                     data_copy[row][col] = (0, 0, 0)
+#         io.imsave(path, data_copy, check_contrast=False)
 # %%
-print('正在运行图像处理：real')
-start_count = 0
-train_processeed_list = get_image_file_list(path_exist('../SLIC_result/60/validation/real/'), True)
-if len(train_processeed_list) != 0:
-    start_count = int(train_processeed_list[-1].split('/')[-1].split('_')[0])
-for i in tqdm(range(start_count, len(real_image_filenames[real_start_count:]))):
-    img = io.imread(real_image_filenames[real_start_count:][i])
-    segments = slic(img, 40, 10)
-    color_dictionary = {}
-    for row in range(segments.shape[0]):
-        for col in range(segments.shape[1]):
-            if segments[row][col] not in color_dictionary:
-                color_dictionary[segments[row][col]] = [(row, col)]
-            else:
-                color_dictionary[segments[row][col]].append((row, col))
+# print('正在运行图像处理：real')
+# start_count = 0
+# train_processeed_list = get_image_file_list(path_exist('../SLIC_result/60/validation/real/'), True)
+# if len(train_processeed_list) != 0:
+#     start_count = int(train_processeed_list[-1].split('/')[-1].split('_')[0])
+# for i in tqdm(range(start_count, len(real_image_filenames[real_start_count:]))):
+#     img = io.imread(real_image_filenames[real_start_count:][i])
+#     segments = slic(img, 40, 10)
+#     color_dictionary = {}
+#     for row in range(segments.shape[0]):
+#         for col in range(segments.shape[1]):
+#             if segments[row][col] not in color_dictionary:
+#                 color_dictionary[segments[row][col]] = [(row, col)]
+#             else:
+#                 color_dictionary[segments[row][col]].append((row, col))
 
-    for color in color_dictionary.keys():
-        path = path_exist(slic_path + 'real/') + "{}_{}.jpg".format(i, color)
-        data_copy = img
-        for row in range(data_copy.shape[0]):
-            for col in range(data_copy.shape[1]):
-                if (row, col) in color_dictionary[color]:
-                    continue
-                else:
-                    data_copy[row][col] = (0, 0, 0)
-        io.imsave(path, data_copy, check_contrast=False)
+#     for color in color_dictionary.keys():
+#         path = path_exist(slic_path + 'real/') + "{}_{}.jpg".format(i, color)
+#         data_copy = img
+#         for row in range(data_copy.shape[0]):
+#             for col in range(data_copy.shape[1]):
+#                 if (row, col) in color_dictionary[color]:
+#                     continue
+#                 else:
+#                     data_copy[row][col] = (0, 0, 0)
+#         io.imsave(path, data_copy, check_contrast=False)
 
 # 生成验证集生成器
 def generate_validation(batch_size: int = 32, target_size: (int, int) = (300, 300)):
